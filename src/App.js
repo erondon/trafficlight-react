fc91b79ed1c9ca2fe2db50551fa750c880837c03
Eleazar <erondon@gmail.com>
2019-08-17 10:44:02 -0400

    done.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function App() {
+class App extends React.Component {
+
+constructor (){
+  super();
+  this.state = {
+    selectedLight: null
+  };
+}
+render (){
+  console.log(this.state);
+  let selRed = '';
+  let selYellow = '';
+  let selGreen = '';
+
+  if (this.state.selectedLight == 'red') {selRed = 'selected'};
+  if (this.state.selectedLight == 'yellow') {selYellow = 'selected'};
+  if (this.state.selectedLight == 'green') {selGreen = 'selected'};
+
   return (
-      <div className='container'>
-        <div className='red light'></div>
-        <div className='yellow light'></div>
-        <div className='green light'></div>
+    
+    <div className='container'>
+        <div className={'red light '+ selRed} onClick={()=> this.setState({selectedLight: 'red'})}></div>
+        <div className={'yellow light '+ selYellow} onClick={()=> this.setState({selectedLight: 'yellow'})}></div>
+        <div className={'green light '+ selGreen} onClick={()=> this.setState({selectedLight: 'green'})}></div>
       </div>
   );
+
+}
 }
 
 export default App;
